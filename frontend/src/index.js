14b6e78f9db62db1f6f12d4c256b9ea453f48230
Rachna Poonit <rachnapoonit11@gmail.com>
2025-03-31 13:27:02 -0400

    FM-42: Apollo client bug - access to backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from 'apollo-upload-client'; // For handling file uploads
 
-const graphqlUri = process.env.REACT_APP_API_URL || 'http://localhost:4000/graphql';
+const graphqlUri = `${process.env.REACT_APP_API_URL || "http://localhost:4000"}/graphql`;
 
 const uploadLink = createUploadLink({
   uri: graphqlUri,
